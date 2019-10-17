15e684766ec6d842c74f8f96cc718b00a1d1abb7
SergioSegaty <49919279+SergioSegaty@users.noreply.github.com>
2019-10-17 15:35:57 -0300

    Criado um ValidadorEmail e um ValidadorEmailTeste

---
+++ AvaliacaoCore/RegraDeNegocio/Validacoes/Cadastro/ValidadorEmail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AvaliacaoCore.DB.Model;
+
+namespace AvaliacaoCore.RegraDeNegocio.Validacoes.Cadastro
+{
+    public class ValidadorEmail : IValidacao<DB.Model.Cadastro>
+    {
+        public ResultadoValidacao Validar(DB.Model.Cadastro model)
+        {
+            if (string.IsNullOrWhiteSpace(model.Email))
+                return Invalido();
+            if (!model.Email.Contains("@"))
+                return Invalido();
+            if (!model.Email.Contains(".com"))
+                return Invalido();
+            return Valido();
+            
+        }
+
+        public ResultadoValidacao Valido()
+        {
+            return new ResultadoValidacao
+            {
+                Valido = true
+            };
+
+        }
+
+
+        public ResultadoValidacao Invalido()
+        {
+            return new ResultadoValidacao
+            {
+                Valido = false,
+                Mensagem = "Esperado Endereço de Email"
+            };
+        }
+    }
+}
---
+++ UnitTestAvaliacao/Core/RegraDeNegocio/Validacoes/Cadastro/ValidadorEmailTeste.cs
@@ -0,0 +1,67 @@
+﻿using AvaliacaoCore.RegraDeNegocio.Validacoes.Cadastro;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestAvaliacao.Core.RegraDeNegocio.Validacoes.Cadastro
+{
+    [TestClass]
+    public class ValidadorEmailTeste
+    {
+        [TestMethod]
+        public void dado_um_cadastro_sem_email_deve_ser_invalido()
+        {
+
+            var cadastro = new AvaliacaoCore.DB.Model.Cadastro();
+            var validador = new ValidadorEmail();
+            var resultado = validador.Validar(cadastro);
+
+            Assert.IsFalse(resultado.Valido);
+            StringAssert.Contains(resultado.Mensagem, "Esperado Endereço de Email");
+        }
+
+        [TestMethod]
+        public void dado_um_cadastro_sem_arroba_email_deve_ser_invalido()
+        {
+            var cadastro = new AvaliacaoCore.DB.Model.Cadastro();
+            cadastro.Email = "alguumacoisa.coisa";
+            var validador = new ValidadorEmail();
+            var resultado = validador.Validar(cadastro);
+
+            Assert.IsFalse(resultado.Valido);
+            StringAssert.Contains(resultado.Mensagem, "Esperado Endereço de Email");
+
+        }
+
+
+        [TestMethod]
+        public void dado_um_cadastro_sem_ponto_com_email_deve_ser_invalido()
+        {
+            var cadastro = new AvaliacaoCore.DB.Model.Cadastro();
+            cadastro.Email = "alguumacoisa@coisa";
+            var validador = new ValidadorEmail();
+            var resultado = validador.Validar(cadastro);
+
+            Assert.IsFalse(resultado.Valido);
+            StringAssert.Contains(resultado.Mensagem, "Esperado Endereço de Email");
+
+        }
+
+
+
+        [TestMethod]
+        public void dado_um_cadastro_com_email_deve_ser_valido()
+        {
+            var cadastro = new AvaliacaoCore.DB.Model.Cadastro();
+            cadastro.Email = "algumacoisa@alguma.com.br";
+            var validador = new ValidadorEmail();
+            var resultado = validador.Validar(cadastro);
+
+            Assert.IsTrue(resultado.Valido);
+        }
+
+
+
+    }
+}
