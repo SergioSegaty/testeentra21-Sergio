b184125bad7d99f7fce3b05f41c3e04ba575253a
SergioSegaty <49919279+SergioSegaty@users.noreply.github.com>
2019-10-21 15:22:59 -0300

    Adicinado no Repositorio o calculo de Imposto de Renda.

--- AvaliacaoCore/DB/ManipuladorSqlite.cs
+++ AvaliacaoCore/DB/ManipuladorSqlite.cs
@@ -18,7 +18,7 @@ namespace AvaliacaoCore.DB
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionbuilder)
         {
-            optionbuilder.UseSqlite(@"Data Source="+ Constantes.NomeArquivoBanco);
+            optionbuilder.UseSqlite(@"Data Source=" + Constantes.NomeArquivoBanco);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -32,6 +32,62 @@ namespace AvaliacaoCore.DB
         {
             cadastro.HoraCadastro = DateTime.Now;
             //cadastro.UF = 
+            double? calcImpostoRenda = 0.0;
+            // Calcular Imposto de Renda
+            var salario = cadastro.Salario;
+            if (salario < 1903.98)
+            {
+                cadastro.ImpostoRenda = 0;
+            }
+            if(salario > 1903.98)
+            {
+                calcImpostoRenda += (salario * 0.075);
+                if (calcImpostoRenda < 142.80)
+                {
+                    cadastro.ImpostoRenda += calcImpostoRenda;
+                }
+                else
+                {
+                    cadastro.ImpostoRenda += 142.80;
+                }
+            }
+            else if( salario > 2826.66)
+            {
+                calcImpostoRenda += (salario * 0.15);
+                if (calcImpostoRenda < 354.80)
+                {
+                    cadastro.ImpostoRenda += calcImpostoRenda;
+                }
+                else
+                {
+                    cadastro.ImpostoRenda += 354.80;
+                }
+            }
+            if( salario > 3751.06)
+            {
+                calcImpostoRenda += (salario * 0.225);
+                if (calcImpostoRenda < 636.13)
+                {
+                    cadastro.ImpostoRenda += calcImpostoRenda;
+                }
+                else
+                {
+                    cadastro.ImpostoRenda += 636.13;
+                }
+            }
+            if(salario > 4664.68)
+            {
+                calcImpostoRenda += (salario * 0.275);
+                if (calcImpostoRenda < 869.36)
+                {
+                    cadastro.ImpostoRenda += calcImpostoRenda;
+                }
+                else
+                {
+                    cadastro.ImpostoRenda += 636.13;
+                }
+            }
+
             Cadastros.Add(cadastro);
             SaveChanges();
         }
