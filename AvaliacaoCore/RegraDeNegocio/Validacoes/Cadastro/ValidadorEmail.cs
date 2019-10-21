1bfb9cd17916e9b26de0a8dd9ea910c1ca674604
SergioSegaty <49919279+SergioSegaty@users.noreply.github.com>
2019-10-21 15:22:06 -0300

    Novo validador Email Via Regex.

--- AvaliacaoCore/RegraDeNegocio/Validacoes/Cadastro/ValidadorEmail.cs
+++ AvaliacaoCore/RegraDeNegocio/Validacoes/Cadastro/ValidadorEmail.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using AvaliacaoCore.DB.Model;
 
 namespace AvaliacaoCore.RegraDeNegocio.Validacoes.Cadastro
@@ -9,14 +10,28 @@ namespace AvaliacaoCore.RegraDeNegocio.Validacoes.Cadastro
     {
         public ResultadoValidacao Validar(DB.Model.Cadastro model)
         {
-            if (string.IsNullOrWhiteSpace(model.Email))
+
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            if (model.Email != null)
+            {
+                Match match = regex.Match(model.Email);
+                if (match.Success)
+                {
+                    return Valido();
+                }
                 return Invalido();
-            if (!model.Email.Contains("@"))
-                return Invalido();
-            if (!model.Email.Contains(".com"))
-                return Invalido();
-            return Valido();
-            
+            }
+            return Invalido();
+
+
+            //if (string.IsNullOrWhiteSpace(model.Email))
+            //    return Invalido();
+            //if (!model.Email.Contains("@"))
+            //    return Invalido();
+            //if (!model.Email.Contains(".com"))
+            //    return Invalido();
+            //return Valido();
+
         }
 
         public ResultadoValidacao Valido()
