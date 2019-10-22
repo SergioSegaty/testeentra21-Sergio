cf0d30360733423c9c98d07f971408ed7b6b18c8
SergioSegaty <49919279+SergioSegaty@users.noreply.github.com>
2019-10-22 00:30:16 -0300

    Atualização do UnitTeste
    Implementei os novos testes.

--- UnitTestAvaliacao/Core/RegraDeNegocio/Validacoes/Cadastro/ValidadorEmailTeste.cs
+++ UnitTestAvaliacao/Core/RegraDeNegocio/Validacoes/Cadastro/ValidadorEmailTeste.cs
@@ -34,6 +34,21 @@ namespace UnitTestAvaliacao.Core.RegraDeNegocio.Validacoes.Cadastro
 
         }
 
+        [TestMethod]
+        public void dado_um_cadastro_com_2_arroba_deve_ser_invalido()
+        {
+            var cadastro = new AvaliacaoCore.DB.Model.Cadastro();
+            cadastro.Email = "alguma@outra@coisa.coisa";
+            var validador = new ValidadorEmail();
+            var resultado = validador.Validar(cadastro);
+
+            Assert.IsFalse(resultado.Valido);
+            StringAssert.Contains(resultado.Mensagem, "Esperado Endereço de Email");
+
+        }
+
+
+
 
         [TestMethod]
         public void dado_um_cadastro_sem_ponto_com_email_deve_ser_invalido()
@@ -47,14 +62,23 @@ namespace UnitTestAvaliacao.Core.RegraDeNegocio.Validacoes.Cadastro
             StringAssert.Contains(resultado.Mensagem, "Esperado Endereço de Email");
 
         }
-
-
-
+        
         [TestMethod]
         public void dado_um_cadastro_com_email_deve_ser_valido()
         {
             var cadastro = new AvaliacaoCore.DB.Model.Cadastro();
             cadastro.Email = "algumacoisa@alguma.com.br";
+            var validador = new ValidadorEmail();
+            var resultado = validador.Validar(cadastro);
+
+            Assert.IsTrue(resultado.Valido);
+        }
+
+        [TestMethod]
+        public void dado_um_cadastro_com_email_provedor_diferente_deve_ser_valido()
+        {
+            var cadastro = new AvaliacaoCore.DB.Model.Cadastro();
+            cadastro.Email = "algumacoisa@rh.shelcorp";
             var validador = new ValidadorEmail();
             var resultado = validador.Validar(cadastro);
 
